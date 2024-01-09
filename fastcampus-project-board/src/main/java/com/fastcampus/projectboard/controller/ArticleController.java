dd907e1ca158114ff7f15365a937ae63a0f345f8
leeheeweon <ggoddii@kaoni.com>
2024-01-09 11:03:35 +0900

    #11 스프링 시큐리티 인증 구현

--- fastcampus-project-board/src/main/java/com/fastcampus/projectboard/controller/ArticleController.java
+++ fastcampus-project-board/src/main/java/com/fastcampus/projectboard/controller/ArticleController.java
@@ -4,7 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -25,4 +27,5 @@ public class ArticleController {
         return "articles/detail";
 
     }
+
 }
