7f9b8d94114d36ccaacd90ef713186e60094dc68
leeheeweon <ggoddii@kaoni.com>
2024-01-10 15:18:45 +0900

    #18 테스트 코드 추가

--- fastcampus-project-board/src/test/java/com/fastcampus/projectboard/service/ArticleCommentServiceTest.java
+++ fastcampus-project-board/src/test/java/com/fastcampus/projectboard/service/ArticleCommentServiceTest.java
@@ -30,7 +30,6 @@ class ArticleCommentServiceTest {
     @Mock
     private ArticleRepository articleRepository;
 
-
     @DisplayName("게시글 ID로 조회하면, 해당하는 댓글 리스트를 반환한다.")
     @Test
     void givenArticleId_whenSearchingArticleComments_thenReturnsArticleComments() throws Exception {
