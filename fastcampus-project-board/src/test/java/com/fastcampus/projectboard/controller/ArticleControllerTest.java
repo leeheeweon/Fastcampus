d6aa3852b3faa6037cb722527f6b86fa1a1bb665
leeheeweon <ggoddii@kaoni.com>
2024-01-12 09:37:49 +0900

    게시판 페이지 기능 구현2

--- fastcampus-project-board/src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
+++ fastcampus-project-board/src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
@@ -1,15 +1,28 @@
 package com.fastcampus.projectboard.controller;
 
 import com.fastcampus.projectboard.config.SecurityConfig;
+import com.fastcampus.projectboard.dto.ArticleWithCommentsDto;
+import com.fastcampus.projectboard.dto.UserAccountDto;
+import com.fastcampus.projectboard.service.ArticleService;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import static org.mockito.BDDMockito.any;
+import static org.mockito.BDDMockito.eq;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
@@ -22,14 +35,19 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class ArticleControllerTest {
     private final MockMvc mvc;
 
-    public ArticleControllerTest(@Autowired MockMvc mvc) {
+    @MockBean
+    private ArticleService articleService;
+
+    public ArticleControllerTest(@Autowired MockMvc mvc, ArticleService articleService) {
         this.mvc = mvc;
+        this.articleService = articleService;
     }
 
     @DisplayName("[view] [GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
         //Given
+        given(articleService.searchArticles(eq(null), eq(null), any(Pageable.class))).willReturn(Page.empty());
 
         //When
 
@@ -40,13 +58,15 @@ class ArticleControllerTest {
                 .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("articles"));
 
+        then(articleService).should().searchArticles(eq(null), eq(null), any(Pageable.class));
     }
 
     @DisplayName("[view] [GET] 게시글 리스트 상세 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingArticlesViewDetail_thenReturnsArticlesView() throws Exception {
         //Given
-
+        Long articleId = 1L;
+        given(articleService.getArticle(articleId)).willReturn(createArticleWithCommentsDto());
         //When
 
         //Then
@@ -56,7 +76,9 @@ class ArticleControllerTest {
                 .andExpect(view().name("articles/detail"))
                 .andExpect(model().attributeExists("article"))
                 .andExpect(model().attributeExists("articleComments"));
+        then(articleService).should().getArticle(articleId);
     }
+
 
     @Disabled("구현 중")
     @DisplayName("[view] [GET] 게시글 리스트 검색 전용 페이지 - 정상 호출")
@@ -86,5 +108,35 @@ class ArticleControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.TEXT_HTML))
                 .andExpect(model().attributeExists("articles/search-hashtag"));
+    }
+
+    private ArticleWithCommentsDto createArticleWithCommentsDto() {
+        return ArticleWithCommentsDto.of(
+                1L,
+                createUserAccountDto(),
+                Set.of(),
+                "title",
+                "content",
+                null,
+                LocalDateTime.now(),
+                "heeweon",
+                LocalDateTime.now(),
+                "heeweon"
+        );
+    }
+
+
+    private UserAccountDto createUserAccountDto() {
+        return UserAccountDto.of(
+                "1L",
+                "pw",
+                "ggoddii@naver.com",
+                "heeweon",
+                "memo",
+                LocalDateTime.now(),
+                "heeweon",
+                LocalDateTime.now(),
+                "heeweon"
+        );
     }
 }
